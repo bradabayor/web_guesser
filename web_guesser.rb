a2c9b92625086da3adff619e68850653cae78b57
Brad Swan <bradj.swan@gmail.com>
2018-07-04 21:29:25 +1000

    I3: numbe guess complete

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,24 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-number = rand(100)
+SECRET_NUMBER = rand(100)
+
+def check_guess(num)
+	if num.nil?
+		"Pick a Number"
+	elsif num == SECRET_NUMBER
+		"You Got It Right!"
+	elsif num > SECRET_NUMBER
+		"Too High"
+	elsif num < SECRET_NUMBER
+		"Too Low"
+	else
+		return nil
+	end
+end
 
 get '/' do
-	erb :index, :locals => {:number => number}
+	message = check_guess(params["guess"].to_i)
+	erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
 end
+
