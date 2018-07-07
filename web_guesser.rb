93b3d131905119fbcde4c059c72a1ef5572fa427
Brad Swan <bradj.swan@gmail.com>
2018-07-07 10:53:36 +1000

    I4: added extensions

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,19 +1,45 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-set :SECRET_NUMBER, rand(100)
+def generate_rand
+	rand(100)
+end
+
+secret_number = generate_rand
+guesses_remaining = 5
+
+get '/' do
+	guess = params["guess"]
+	message = check_guess(guess)
+	color = change_bg_color(guess)
+
+	params["cheat"] == true ? cheat = false : cheat = true
+
+	guesses_remaining =- 1
+
+	if guesses_remaining == 0
+		generate_secret_number
+		guesses_remaining = 5
+	end
+
+	erb :index, :locals => {:number => secret_number, :message => message, :color => color, :cheat => cheat}
+end
+
+def generate_rand
+	rand(100)
+end
 
 def check_guess(num)
 	if num.nil?
 		"Pick a Number"
 	else
-		if num.to_i < (SECRET_NUMBER - 5)
+		if num.to_i < (secret_number - 5)
 			"Way Too Low"
-		elsif num.to_i > (SECRET_NUMBER + 5)
+		elsif num.to_i > (secret_number + 5)
 			"Way Too High"	
-		elsif num.to_i > SECRET_NUMBER
+		elsif num.to_i > secret_number
 			"Too High"
-		elsif num.to_i < SECRET_NUMBER
+		elsif num.to_i < secret_number
 			"Too Low"
 		else
 			"Congratulations, You Guessed the Number!"
@@ -21,9 +47,20 @@ def check_guess(num)
 	end
 end
 
-get '/' do
-	guess = params["guess"]
-	message = check_guess(guess)
-	erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
+def change_bg_color(num)
+	if num.nil?
+		"white"
+	else
+		if num.to_i < (secret_number - 5)
+			"red"
+		elsif num.to_i > (secret_number + 5)
+			"red"	
+		elsif num.to_i > secret_number
+			"yellow"
+		elsif num.to_i < secret_number
+			"yellow"
+		else
+			"green"
+		end
+	end
 end
-
